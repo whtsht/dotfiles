5633b42126e0356617b9ec7149e9380072cd2da1
whtsht <whiteshirt0079@gmail.com>
2024-06-09 09:32:05 +0900

    update

--- config/nvim/lua/user/neovide.lua
+++ config/nvim/lua/user/neovide.lua
@@ -1,4 +1,4 @@
-vim.g.neovide_scale_factor = 1.3
+vim.g.neovide_scale_factor = 1.1
 vim.g.neovide_scroll_animation_length = 0.1
-vim.g.neovide_cursor_trail_size = 0.4
+vim.g.neovide_cursor_trail_size = 0.3
 vim.g.neovide_cursor_vfx_mode = "ripple"
