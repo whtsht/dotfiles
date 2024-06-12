74e4f4fcfdc78795d310f5ae7044244b463207b2
whtsht <whiteshirt0079@gmail.com>
2024-06-12 21:51:01 +0900

    update

--- config/nvim/lua/user/autocmd.lua
+++ config/nvim/lua/user/autocmd.lua
@@ -1,15 +1,24 @@
 vim.api.nvim_create_augroup("userconfig", {})
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = "userconfig",
+  desc = "automatically format files when saving",
+  pattern = "*.rs,*.go,*.java",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	group = "userconfig",
-	desc = "return cursor to where it was last time closing the file",
-	pattern = "*",
-	command = 'silent! normal! g`"zv',
+  group = "userconfig",
+  desc = "return cursor to where it was last time closing the file",
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
 })
 
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = "userconfig",
-	desc = "automatically disable spell check when enter term mode",
-	pattern = "*",
-	command = "setlocal nospell | startinsert",
+  group = "userconfig",
+  desc = "automatically disable spell check when enter term mode",
+  pattern = "*",
+  command = "setlocal nospell | startinsert",
 })
