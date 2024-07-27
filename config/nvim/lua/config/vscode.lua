47c5d54f045ccd9588075065c43dce5fae53f8cc
whtsht <whiteshirt0079@gmail.com>
2024-07-27 18:51:11 +0900

    update

---
+++ config/nvim/lua/config/vscode.lua
@@ -0,0 +1,3 @@
+if vim.g.vscode then
+  vim.opt.spell = false
+end
--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,23 @@
-return {
-  -- disable plugins
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-}
+if vim.g.vscode then
+  return {
+    -- disable plugins
+    { "nvimdev/dashboard-nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
+    { "rafamadriz/friendly-snippets", enabled = false },
+    { "shortcuts/no-neck-pain.nvim", enabled = false },
+    { "nvim-telescope/telescope.nvim", enabled = false },
+    { "williamboman/mason-lspconfig.nvim", enabled = false },
+    { "vim-skk/skkeleton", enabled = false },
+    { "rcarriga/nvim-notify", enabled = false },
+    { "folke/noice.nvim", enabled = false },
+    { "nvim-lualine/lualine.nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
+  }
+else
+  return {
+    -- disable plugins
+    { "nvimdev/dashboard-nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
+    { "rafamadriz/friendly-snippets", enabled = false },
+  }
+end
