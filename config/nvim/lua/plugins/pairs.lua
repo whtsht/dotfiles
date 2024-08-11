20c9318c52779b1ac96c1344080db00f6993b874
whtsht <whiteshirt0079@gmail.com>
2024-08-11 12:04:16 +0900

    update

--- config/nvim/lua/plugins/pairs.lua
+++ config/nvim/lua/plugins/pairs.lua
@@ -1,22 +1,23 @@
 return {
   {
     "echasnovski/mini.pairs",
-    config = function()
-      require("mini.pairs").setup({
-        mappings = {
-          ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-          ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-          ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
-          [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-          ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-          ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-
-          ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-          -- ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-          -- ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
-        },
-      })
+        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+        ["'"] = false,
+        ["`"] = false,
+      },
+    },
+    config = function(_, opts)
+      LazyVim.mini.pairs(opts)
     end,
   },
 }
