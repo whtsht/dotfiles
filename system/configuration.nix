6a2f92189ef337a9a412e0e839321ede7ada2a02
whtsht <whiteshirt0079@gmail.com>
2024-08-16 00:03:27 +0900

    update

--- system/configuration.nix
+++ system/configuration.nix
@@ -6,11 +6,6 @@
       /etc/nixos/hardware-configuration.nix
     ];
 
-  # Bootloader.
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "/dev/nvme0n1";
-  # boot.loader.grub.useOSProber = true;
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
