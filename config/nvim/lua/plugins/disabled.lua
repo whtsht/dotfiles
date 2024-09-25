fdc23c10b84841581bd3b90a589213d09e923248
whtsht <whiteshirt0079@gmail.com>
2024-09-25 11:47:29 +0900

    update

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,27 +1,7 @@
-if vim.g.vscode then
-  return {
-    { "nvimdev/dashboard-nvim", enabled = false },
-    { "akinsho/bufferline.nvim", enabled = false },
-    { "rafamadriz/friendly-snippets", enabled = false },
-    { "shortcuts/no-neck-pain.nvim", enabled = false },
-    { "nvim-telescope/telescope.nvim", enabled = false },
-    { "williamboman/mason-lspconfig.nvim", enabled = false },
-    { "rcarriga/nvim-notify", enabled = false },
-    { "folke/noice.nvim", enabled = false },
-    { "nvim-lualine/lualine.nvim", enabled = false },
-    { "akinsho/bufferline.nvim", enabled = false },
-    { "lukas-reineke/indent-blankline.nvim", enabled = false },
-    { "hrsh7th/nvim-cmp", enabled = false },
-    { "zbirenbaum/copilot.lua", enabled = false },
-    { "zbirenbaum/copilot-cmp", enabled = false },
-    { "echasnovski/mini.pairs", enabled = false },
-  }
-else
-  return {
-    { "nvimdev/dashboard-nvim", enabled = false },
-    { "akinsho/bufferline.nvim", enabled = false },
-    { "rafamadriz/friendly-snippets", enabled = false },
-    { "echasnovski/mini.pairs", enabled = false },
-    { "rcarriga/nvim-notify", enabled = false },
-  }
-end
+return {
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
