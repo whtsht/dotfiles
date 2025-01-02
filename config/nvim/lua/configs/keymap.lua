d4ea2d82bfc28a6c2ce412b06d807099dc3454d4
whtsht <whiteshirt0079@gmail.com>
2025-01-03 00:36:59 +0900

    update

--- config/nvim/lua/configs/keymap.lua
+++ config/nvim/lua/configs/keymap.lua
@@ -37,6 +37,7 @@ set_keymap("n", "<leader>fc", "<cmd>FzfLua command_history<cr>")
 set_keymap("n", "<leader>fg", "<cmd>FzfLua git_files<cr>")
 set_keymap("n", "<leader>ft", "<cmd>FzfLua git_branches<cr>")
 set_keymap("n", "<leader>fl", "<cmd>FzfLua live_grep_native<cr>")
+set_keymap("n", "<leader>fr", "<cmd>FzfLua resume<cr>")
 
 -- Luasnip
 vim.keymap.set("i", "<Tab>", function()
