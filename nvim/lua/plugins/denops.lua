d17fb2fcd1cc6b33d34049b1d9dd8b2919e0ac43
whtsht <whiteshirt0079@gmail.com>
2026-04-04 22:08:40 +0900

    remove some plugins

--- nvim/lua/plugins/denops.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "vim-denops/denops.vim",
-  lazy = false,
-  dependencies = {
-    "vim-denops/denops-helloworld.vim"
-  }
-}
--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,5 +1,3 @@
 return {
   'j-hui/fidget.nvim',
-  opts = {
-  }
 }
