b39f799c09ac94b1c2d513bc9b123b07aa7b025f
whtsht <whiteshirt0079@gmail.com>
2026-04-04 21:29:13 +0900

    telescope resume

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -27,11 +27,13 @@ return {
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fc', builtin.git_status, { desc = 'Telescope git status' })
     vim.keymap.set('n', '<leader>fl', telescope.extensions.live_grep_args.live_grep_args,
       { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>ft', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = 'Telescope switch git branches' })
+    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Telescope resume' })
   end
 }
