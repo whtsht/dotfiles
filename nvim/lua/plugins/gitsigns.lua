1ef91a63107b0131616fadc014e9faf816a2c6de
whtsht <whiteshirt0079@gmail.com>
2026-04-04 21:29:05 +0900

    move git diff

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,12 @@
 return {
-  'lewis6991/gitsigns.nvim'
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>",
+      { desc = "Go to next git hunk", silent = true }
+    )
+    vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>",
+      { desc = "Go to previous git hunk", silent = true }
+    )
+  end,
 }
