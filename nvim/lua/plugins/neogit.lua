469b3c873764f363a7b857a4efe6369c004bf31b
whtsht <whiteshirt0079@gmail.com>
2026-04-12 17:23:57 +0900

    git in neovim

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return{
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "m00qek/baleia.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  }
+}
