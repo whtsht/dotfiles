24a20a4b6060f35714ce390bc8dcedb9db682740
whtsht <whiteshirt0079@gmail.com>
2026-04-12 21:42:52 +0900

    gitsigns keymap

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,11 +2,14 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup()
-    vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>",
+    vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>",
       { desc = "Go to next git hunk", silent = true }
     )
-    vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>",
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<CR>",
       { desc = "Go to previous git hunk", silent = true }
+    )
+    vim.keymap.set("n", "<leader>gi", ":Gitsigns preview_hunk_inline<CR>",
+      { desc = "Preview git hunk inline", silent = true }
     )
   end,
 }
